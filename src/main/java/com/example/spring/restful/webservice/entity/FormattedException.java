db27868b5fb3a53c026501240c7c2b19a6a99caf
Pankaj Bagal <pankajsbagal@gmail.com>
2021-03-23 13:32:59 +0530

    added customized exception format

---
+++ src/main/java/com/example/spring/restful/webservice/entity/FormattedException.java
@@ -0,0 +1,20 @@
+package com.example.spring.restful.webservice.entity;
+
+import java.util.Date;
+
+
+public class FormattedException {
+    private Date timestamp;
+    private String message;
+    private String description;
+
+    public FormattedException(Date timestamp, String message, String description) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.description = description;
+    }
+
+    public static FormattedException formatException(Date timestamp, String message, String description){
+        return new FormattedException(timestamp,message,description);
+    }
+}
