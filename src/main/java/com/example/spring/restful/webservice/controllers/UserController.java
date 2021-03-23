92b80f70114afef7c5a3e3123ad5f83cffee151a
Pankaj Bagal <pankaj.bagal@ideas.com>
2021-03-23 06:30:47 -0600

    added - delete request for user deletion

--- src/main/java/com/example/spring/restful/webservice/controllers/UserController.java
+++ src/main/java/com/example/spring/restful/webservice/controllers/UserController.java
@@ -4,12 +4,8 @@ import com.example.spring.restful.webservice.entity.User;
 import com.example.spring.restful.webservice.exceptions.UserNotFoundException;
 import com.example.spring.restful.webservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,17 +20,22 @@ public class UserController {
         return userService.findAll();
     }
 
-    @GetMapping(path="{id}")
-    public User findOne(@PathVariable Integer id){
+    @GetMapping(path = "{id}")
+    public User findOne(@PathVariable Integer id) {
         final User user = userService.findOne(id);
-        if(user==null) {
+        if (user == null) {
             throw new UserNotFoundException(id);
         }
         return user;
     }
 
     @PostMapping(path = "/save")
-    public void save(@RequestBody User user){
+    public void save(@RequestBody User user) {
         userService.save(user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public User deleteById(@PathVariable Integer id) {
+        return userService.deleteById(id);
     }
 }
--- src/main/java/com/example/spring/restful/webservice/service/UserService.java
+++ src/main/java/com/example/spring/restful/webservice/service/UserService.java
@@ -5,18 +5,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class UserService {
     private static final List<User> users = new ArrayList<>();
-    public static int USER_COUNT=0;
+    public static int USER_COUNT = 0;
+
     static {
-        users.add(new User(1, "A",new Date()));
-        users.add(new User(2, "B",new Date()));
-        users.add(new User(3, "C",new Date()));
-        USER_COUNT=3;
-    };
+        users.add(new User(1, "A", new Date()));
+        users.add(new User(2, "B", new Date()));
+        users.add(new User(3, "C", new Date()));
+        USER_COUNT = 3;
+    }
+
+    ;
 
     public List<User> findAll() {
         return users;
@@ -25,10 +29,22 @@ public class UserService {
     public User save(User user) {
         users.add(user);
         ++USER_COUNT;
-        return users.get(USER_COUNT-1);
+        return users.get(USER_COUNT - 1);
     }
 
-    public User findOne(Integer id){
-        return users.stream().filter(u-> u.getId().equals(id)).findFirst().orElse(null);
+    public User deleteById(Integer id) {
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+            if (user.getId().equals(id)) {
+                iterator.remove();
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User findOne(Integer id) {
+        return users.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
     }
 }
