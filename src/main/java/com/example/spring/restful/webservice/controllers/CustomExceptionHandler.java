fbf4d4da1cbb9fe2eaa7b196fb8c0d6db9e4477d
Pankaj Bagal <pankaj.bagal@ideas.com>
2021-03-23 04:47:05 -0600

    added custom exception handling

---
+++ src/main/java/com/example/spring/restful/webservice/controllers/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.spring.restful.webservice.controllers;
+
+import com.example.spring.restful.webservice.entity.FormattedException;
+import com.example.spring.restful.webservice.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<FormattedException> handleUserNotFoundException(Exception ex, WebRequest request){
+        FormattedException formattedException = FormattedException.formatException(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(formattedException,HttpStatus.NOT_FOUND);
+    }
+}
